d1c3291f921e4fbd20131d19de3fd5dc7f43e009
robert <robert@sherman.one>
2024-06-25 16:38:13 -0400

    added basic rate limiting and moved to config file

--- src/routes/ip.ts
+++ src/routes/ip.ts
@@ -1,7 +1,11 @@
 import { FastifyPluginAsync, RequestGenericInterface } from "fastify";
-const config = {api: 'ip-api'}
-const access_key = 'asdfadfas'
+const config = require('../config.json')
 const cache: {[ip_address: string]: string} = {}
+const rateLimitCount: RateCounter = {ipapi: {}, ipstack: {}}
+interface RateCounter {
+    ipapi: {[hour: string]: number}
+    ipstack: {[hour: string]: number}
+}
 
 const routes: FastifyPluginAsync = async (fastify, opts) => {
     interface RequestGet extends RequestGenericInterface {
@@ -28,22 +32,41 @@ const routes: FastifyPluginAsync = async (fastify, opts) => {
     })
 }
 
-async function getCountry(ip: string): Promise<string> {
+export async function getCountry(ip: string): Promise<string> {
     if (ip in cache) {
         return cache[ip]
     }
-    if (config.api === 'ip-api'){
+    // this method has a potential bug if it's called exactly 24 hours apart
+    const hour = new Date().getHours().toString()
+    try {
+        if (rateLimitCount.ipapi?.[hour] >= config.apiRateLimits.ipapi ) {
+            // TODO make rate limit error
+            throw Error()
+        } else if (rateLimitCount.ipapi?.[hour]) {
+            rateLimitCount.ipapi[hour] += 1
+        } else {
+            rateLimitCount.ipapi = {[hour]: 1}
+        }
         const resp = await (await fetch(`http://ip-api.com/json/${ip}`)).json() as {country: string}
         cache[ip] = resp.country
         return resp.country
-    } else if (config.api === 'ipstack') {
-        const resp = await (await fetch(`https://api.ipstack.com/${ip}?access_key=${access_key}`)).json() as {country_name: string}
-        cache[ip] = resp.country_name
-        return resp.country_name
-    } else {
-        throw Error('unconfigured api option')
+    } catch (e) {
+        try {
+            if (rateLimitCount.ipstack?.[hour] >= config.apiRateLimits.ipstack ) {
+                // TODO make rate limit error
+                throw Error()
+            } else if (rateLimitCount.ipstack?.[hour]) {
+                rateLimitCount.ipstack[hour] += 1
+            } else {
+                rateLimitCount.ipstack = {[hour]: 1}
+            }
+            const resp = await (await fetch(`https://api.ipstack.com/${ip}?access_key=${config.accessKey}`)).json() as {country_name: string}
+            cache[ip] = resp.country_name
+            return resp.country_name
+        } catch (e) {
+            throw new Error('backend error')
+        }
     }
 }
-
 
 export default routes
